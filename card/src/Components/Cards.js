948d6fda82ea84823e1bb84df632f1b3b097b414
Cristina Laryea <crispad06@gmail.com>
2020-03-19 22:43:44 -0400

    file update

--- card/src/Components/Cards.js
+++ card/src/Components/Cards.js
@@ -37,7 +37,7 @@ import { Card, Icon } from 'semantic-ui-react'
 const Cards = () => (
   <Card
     image='/images/avatar/large/elliot.jpg'
-    header='Elliot Baker'
+    header='Joe Jones'
     meta='Friend'
     description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
     Cards={Cards}
